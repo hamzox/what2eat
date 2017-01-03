fec6603b2b616fc0e92b17d760d9d34227ae9d7c
Hamza Khan <hamza_140995@hotmail.com>
2017-01-04 02:39:17 +0500

    Data was console log from firebase, some operations to be performed on view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,7 +4,22 @@ angular.module('starter.controllers', [])
 
   })
 
+
   .controller ('homeCtrl', function ($scope, $firebaseObject) {
+
+    $scope.getSelectedItems=[];
+
+    $scope.isLoaded=false;
+
+    var ref = firebase.database().ref();
+    var obj = $firebaseObject(ref);
+
+    loadData(obj,$scope);
+
+    //fetching json data from firebase url
+    $scope.data = obj;
+    console.log($scope.data);
+
 
     $scope.value=8;
     $scope.selectModel=null;
@@ -29,10 +44,21 @@ angular.module('starter.controllers', [])
 
   })
 
-
   .controller('votesCtrl', function($scope) {
 
   })
-  function homeCtrl() {
 
-  }
+function loadData(obj,scope) {
+  obj.$loaded().then(function() {
+
+    console.log("Loaded record!");
+    scope.isLoaded=true;
+
+    // To iterate the key/value pairs of the object, use angular.forEach()
+    angular.forEach(obj, function(value, key) {
+      console.log(key, value);
+    });
+  })
+}
+
+
